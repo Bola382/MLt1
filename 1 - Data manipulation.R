11dbfda36e3dac80a5ed844c815a2d380bd0342a
Bola382e <marcus.gabriel.silva@hotmail.com>
2023-01-13 21:21:47 -0400

    Saving the data

--- 1 - Data manipulation.R
+++ 1 - Data manipulation.R
@@ -1,25 +1,12 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 rm(list=ls())
-suppressMessages(library(tidyverse));suppressMessages(library(GGally))
 
 # loading dataset
-data = read.table("data/echocardiogram.txt",h=T,sep=",")
-colnames(data)=c("survival","still-alive","age-heart-attack","pericardial-eff",
-                 "fractional-short","epss","lvdd","wms","wmi","mult","name","group",
-                 "alive-at-1")
+data = read.table("data/spambase.txt",h=F,sep=",")
 View(data)
 
-data = data %>% select(-`still-alive`,-name)  
-
-id = which(data=="?",arr.ind = T)
-data[id]=NA
-
-data=na.omit(data)
-data = apply(data,1:2,as.numeric)
-# proportions 
-data %>% select(last_col()) %>% table %>% prop.table %>% barplot(names.arg=c("control","patient"),ylim=c(0,.8))
-
-# correlations by class
-data %>% data.frame %>% mutate(alive.at.1=factor(alive.at.1)) %>% ggpairs(aes(color = `alive.at.1`)) + theme_bw()
+# data set about spam emails
+# description given on data/spambase.names
+# class var 58: 0 not spam, 1 spam
 
 save.image("Data.RData")
