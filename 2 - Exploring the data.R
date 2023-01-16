3425e16489c7971dc19fe81801c0b2c1b9a7845b
Bola382e <marcus.gabriel.silva@hotmail.com>
2023-01-16 00:48:08 -0400

    Exploring variables

--- 2 - Exploring the data.R
+++ 2 - Exploring the data.R
@@ -1,10 +1,15 @@
 rm(list=ls())
 load("Data.RData")
+library(ggplot2);library(GGally)
+library(dplyr)
 rmnames = function(a) {names(a)=NULL;  return(a)}
 
 # describing data
 aa = data %>% select(-last_col()) %>% as.matrix
 bb = data %>% select(last_col()) %>% unlist %>% rmnames
+
+table(bb)
+prop.table(table(bb))
 
 MVN::mvn(aa)
 
@@ -14,10 +19,12 @@ MVN::mvn(aa,mvnTest="royston")$multivariateNormality
 MVN::mvn(aa,mvnTest="dh")$multivariateNormality
 MVN::mvn(aa,mvnTest="energy")$multivariateNormality
 
-vars = c(52, 53,  7, 16, 46, 27, 28, 42, 5, 24, 9, 22, 32, 4, 34)
+vars = c(52, 53,  7, 56, 25, 46, 16, 42, # best vars from variable selection step
+         27, 44, 17, 55, 26, 23, 39, 28,
+         48, 29, 41, 31,  4, 34, 32)
 
 df = data.frame(aa[,vars[1:5]])
-colnames(df)=c("!", "$", "remove","free", "edu")
+colnames(df)=c("!", "$", "remove","maior seq caixa alta", "hp")
 
 ggpairs(df,aes(color = factor(bb))) + theme_bw()
 
